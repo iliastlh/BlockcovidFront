554f47760ac26da50e59c4da1d40b42fcb7997d2
Dorian Gruselin <dorian.gruselin@hotmail.com>
2020-12-09 18:00:29 +0100

    Fix: nom et desc facultatifs dans une QrCard

--- src/components/QrCard.vue
+++ src/components/QrCard.vue
@@ -3,9 +3,9 @@
         <div :id="id" class="card-img-top">
             <VueQrCode tag="img" v-html="qrCode" :value="content" class="w-100"/>
         </div>
-        <div class="card-body">
-            <h5 class="card-title">{{ title }}</h5>
-            <p class="card-text">{{ description }}</p>
+        <div v-if="title || description" class="card-body pt-0">
+            <h5 v-if="title" class="card-title">{{ title }}</h5>
+            <p v-if="description" class="card-text">{{ description }}</p>
         </div>
         <div class="card-footer d-flex justify-content-around">
             <button type="button" class="btn btn-dark" @click="printCode">Imprimer</button>
